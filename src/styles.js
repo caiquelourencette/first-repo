50a568c6e40e903ec130ac2d4fba15e74b7dfc91
caiquelourencette <caique_flr@hotmail.com>
2021-08-01 19:03:52 -0300

    alinhamento do header ao topo

--- src/styles.js
+++ src/styles.js
@@ -6,17 +6,17 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   height: 64px;
   align-items: center;
-  margin: 0 45px;
-
-  img {
-    margin-left: 45px;
-  }
 `;
 
 export const Right = styled.div`
   height: 64px;
-  margin-right: 45px;
   display: flex;
   justify-content: space-between;
   width: 192px;
+`;
+
+export const Header = styled.header`
+  margin: 0 45px;
+  justify-content: flex-start;
+  padding-top: 8px;
 `;
