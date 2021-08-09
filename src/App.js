6fef69570a10a26ab2bff75f98584306e5f437f1
caiquelourencette <caique_flr@hotmail.com>
2021-08-08 22:47:12 -0300

    texto centralizado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { HeaderContainer, Right, Header } from './styles';
+import { HeaderContainer, Right, Header, OurStoryContainer, TextContainer } from './styles';
 
 function App() {
   const teste = () => {
     console.log('botao entrar clicado')
   }
   return (
-    <div className="App">
+    <div className="App"> 
       <Header className="App-header">
-      <HeaderContainer>
-        <img src={logo} height={32} width={108} alt="logo" />
-        <Right>
-          <select>
-            <option value="Português">Português</option>
-            <option value="English">English</option>
-          </select>
-          <button onClick={teste}>
-            Entrar
-          </button>
-        </Right>
-      </HeaderContainer>
+        <HeaderContainer>
+          <img src={logo} height={32} width={108} alt="logo" />
+          <Right>
+            <select>
+              <option value="Português">Português</option>
+              <option value="English">English</option>
+            </select>
+            <button onClick={teste}>
+              Entrar
+            </button>
+          </Right>
+        </HeaderContainer>
       </Header>
+      <OurStoryContainer>
+        <TextContainer>
+          <h1>Filmes, séries e muito mais.</h1>
+          <h1>Sem limites.</h1>
+          <h2>Assista onde quiser. Cancele quando quiser.</h2>
+          <h3>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</h3>
+        </TextContainer>
+      </OurStoryContainer>
     </div>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -19,4 +19,34 @@ export const Header = styled.header`
   margin: 0 45px;
   justify-content: flex-start;
   padding-top: 8px;
+  height: 65px;
+`;
+
+export const OurStoryContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 80vh;
+  align-items: center;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+
+  h1 {
+    font-size: 3.125rem;
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.625rem;
+    margin: 16px 0;
+    font-weight: 400;
+  }
+  h3 {
+    font-size: 23px;
+    margin: 0;
+    font-weight: 400;
+  }
 `;
